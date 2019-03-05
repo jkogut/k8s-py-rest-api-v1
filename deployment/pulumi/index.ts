dc53240ed05ae70d6f8b5e112c2d352c0e5740ec
Jan Kogut <mr.jkogut@gmail.com>
2019-03-05 16:27:35 +0100

    ADD: minimal pulumi config for k8s deployment

---
+++ deployment/pulumi/index.ts
@@ -0,0 +1,35 @@
+import * as k8s from "@pulumi/kubernetes";
+
+// nginx container, replicated 1 time.
+const appName = "pyapi";
+const appImage = "nginx"
+
+const appLabels = { app: appName };
+const nginx = new k8s.apps.v1beta1.Deployment(appName, {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [
+                        { 
+                            name: appName, 
+                            image: appImage,
+                        }] 
+                    }
+        }
+    }
+});
+
+// Allocate an IP to the nginx Deployment.
+const frontend = new k8s.core.v1.Service(appName, {
+    metadata: { labels: nginx.spec.apply(spec => spec.template.metadata.labels) },
+    spec: {
+        type: "LoadBalancer",
+        ports: [{ port: 80, targetPort: 80, protocol: "TCP" }],
+        selector: appLabels
+    }
+});
+
+// When "done", this will print the public IP.
+export let frontendIp = frontend.status.apply(status => status.loadBalancer.ingress[0].ip);
